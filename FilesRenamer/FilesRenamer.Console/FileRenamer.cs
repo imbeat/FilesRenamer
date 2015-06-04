a51a67135192a2cc36191fb91f1832536c470f82
Navoznov Ivan <ivan.navoznov@gmail.com>
2015-06-04 14:00:00 +0300

    рекурсивный обход дерева подпапок, добавлен логгер (пока без логировния)

---
+++ FilesRenamer/FilesRenamer.Console/FileRenamer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace FilesRenamer.Console
+{
+    public class FileRenamer
+    {
+        public const int TotalWidth = 3;
+
+        public void Rename(FileInfo file, int number)
+        {
+            if (file == null) throw new ArgumentNullException("file");
+            var newFileName = number.ToString(CultureInfo.InvariantCulture).PadLeft(TotalWidth, '0');
+            var newFilePath = Path.Combine(file.DirectoryName, newFileName + file.Extension);
+            File.Move(file.FullName, newFilePath);
+        }
+
+        public void RenameFilesInDirectory(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                throw new DirectoryNotFoundException();
+            }
+            var filePathsList = Directory.GetFiles(directoryPath);
+            var subdirectoryPathsList = Directory.GetDirectories(directoryPath);
+            if (subdirectoryPathsList.Any())
+            {
+                foreach (var subdirectoryPath in subdirectoryPathsList)
+                {
+                    RenameFilesInDirectory(subdirectoryPath);
+                }
+            }
+
+            var files = filePathsList.Select(x => new FileInfo(x)).OrderBy(x => x.Name).ToArray();
+            for (var i = 0; i < files.Length; i++)
+            {
+                var file = files[i];
+                Rename(file, i+1);
+            }
+        }
+
+    }
+}
--- FilesRenamer/FilesRenamer.Console/Program.cs
+++ FilesRenamer/FilesRenamer.Console/Program.cs
@@ -1,44 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FilesRenamer.Console
 {
     class Program
     {
-        private const int TotalWidth = 3;
-
         private static void Main(string[] args)
         {
             var directoryPath = GetFolderParh(args);
-
-            RenameFilesInDirectory(directoryPath);
+            var fileRenamer = new FileRenamer();
+            fileRenamer.RenameFilesInDirectory(directoryPath);
         }
 
-        private static void RenameFilesInDirectory(string directoryPath)
-        {
-            if (!Directory.Exists(directoryPath))
-            {
-                throw new DirectoryNotFoundException();
-            }
-            var filePathList = Directory.GetFiles(directoryPath);
-            foreach (var file in filePathList)
-            {
-                System.Console.WriteLine(file);
-            }
-            var files = filePathList.Select(x => new FileInfo(x)).OrderBy(x => x.Name).ToArray();
-            for (int i = 1; i < files.Length+1; i++)
-            {
-                var file = files[i];
-                var extension = file.Extension;
-                var newFileName = i.ToString().PadLeft(TotalWidth, '0');
-                var newFilePath = Path.Combine(directoryPath, newFileName + extension);
-                File.Move(file.FullName, newFilePath);
-            }
-        }
 
         private static string GetFolderParh(string[] args)
         {
